da6cd3c852093738215ffc0c548407eb850b9904
Roei Shchory <roei.shchory@gmail.com>
2024-07-15 22:21:09 +0300

    new spring project

---
+++ src/main/java/com/handson/write_my_groceries_list/WriteMyGroceriesListApplication.java
@@ -0,0 +1,13 @@
+package com.handson.write_my_groceries_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteMyGroceriesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteMyGroceriesListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handson/write_my_groceries_list/WriteMyGroceriesListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handson.write_my_groceries_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteMyGroceriesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
