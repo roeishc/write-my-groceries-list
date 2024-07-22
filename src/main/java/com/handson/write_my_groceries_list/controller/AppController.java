537fce6b44330c86ad23210e40bf9916227aeb3a
Roei Shchory <roei.shchory@gmail.com>
2024-07-22 16:13:30 +0300

    created new aws controller and moved relevant api

--- src/main/java/com/handson/write_my_groceries_list/controller/AppController.java
+++ src/main/java/com/handson/write_my_groceries_list/controller/AppController.java
@@ -5,12 +5,7 @@ import com.handson.write_my_groceries_list.aws.S3BucketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
@@ -21,25 +16,6 @@ public class AppController {
     @Autowired
     S3BucketService s3BucketService;
 
-
-    @GetMapping("/image/{fileName}")
-    public ResponseEntity<byte[]> downloadImage(@PathVariable String fileName){
-        byte[] imageBytes = s3BucketService.downloadImage(fileName);
-        if (imageBytes == null)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.parseMediaType(S3BucketService.determineContentType(fileName)));
-        return new ResponseEntity<>(imageBytes, httpHeaders, HttpStatus.OK);
-    }
-
-    @PostMapping("/image")
-    public ResponseEntity<?> uploadReceiptImage(@RequestParam("image") MultipartFile image){
-        String imageUrl = s3BucketService.uploadImage(image, S3BucketService.generateFileName(image));
-        if (imageUrl == null){
-            return new ResponseEntity<>("Failed to upload image", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(imageUrl, HttpStatus.OK);
-    }
 
     @GetMapping(path = "/helloWorld")
     public String helloWorld(){
---
+++ src/main/java/com/handson/write_my_groceries_list/controller/AwsController.java
@@ -0,0 +1,44 @@
+package com.handson.write_my_groceries_list.controller;
+
+import com.handson.write_my_groceries_list.aws.S3BucketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("/api")
+public class AwsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AwsController.class);
+
+    @Autowired
+    S3BucketService s3BucketService;
+
+
+    @GetMapping("/image/{fileName}")
+    public ResponseEntity<byte[]> downloadImage(@PathVariable String fileName){
+        byte[] imageBytes = s3BucketService.downloadImage(fileName);
+        if (imageBytes == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.parseMediaType(S3BucketService.determineContentType(fileName)));
+        return new ResponseEntity<>(imageBytes, httpHeaders, HttpStatus.OK);
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<?> uploadReceiptImage(@RequestParam("image") MultipartFile image){
+        String imageUrl = s3BucketService.uploadImage(image, S3BucketService.generateFileName(image));
+        if (imageUrl == null){
+            return new ResponseEntity<>("Failed to upload image", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(imageUrl, HttpStatus.OK);
+    }
+
+}
